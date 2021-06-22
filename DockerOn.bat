c62c16f7f92c5c968b93bbf441e759c94e91d651
Javi Garate <fjgarate@cimne.upc.edu>
2021-06-22 17:00:44 +0200

    comment

--- DockerOn.bat
+++ DockerOn.bat
@@ -1,4 +1,4 @@
 docker build -t kratos-linux-builder .
 mv ./kratos-latest-linux-64.tar.gz kratos-old-linux-64.tar.gz
-docker cp kratos-linux-builder:/app/Kratos/kratos-latest-linux-64.tar.gz .
+@REM docker cp kratos-linux-builder:/app/Kratos/kratos-latest-linux-64.tar.gz .
 @REM docker run -it  --entrypoint /bin/sh --name kratos-linux-builder kratos-linux-builder
